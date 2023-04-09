9b4ccdf108fb4df683ffe90ab4f16f3364046d60
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:21:09 +0530

    10-april commit31

---
+++ wap-to-print-the-name-of-the-user-in-double-quotes.-expected-output-format---hello-amit-kumar-a1_6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    char name[20] ;
+    printf("Enter your name sir = ");
+    scanf("%s",name);
+    printf("\"Hello , %s\"",name);
+    return 0;
+}
