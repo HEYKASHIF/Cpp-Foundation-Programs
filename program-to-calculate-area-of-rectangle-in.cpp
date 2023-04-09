922cc014cf615683b7b84b130c5ba79777c84c5b
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:09:06 +0530

    10-april commit23

---
+++ program-to-calculate-area-of-rectangle-in.cpp
@@ -0,0 +1,11 @@
+//program to calculate area of rectangle.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<" Enter length and breadth of the rectangle  = "<<endl;
+    cin>>a>>b;
+    cout<<a*b<<" is the area of the rectangle.";
+    return 0;
+}
