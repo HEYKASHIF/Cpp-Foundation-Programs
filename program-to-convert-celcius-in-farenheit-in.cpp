6e7d19f91e28616974186a2848d9de76ad7ce50f
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:03:59 +0530

    10-april commit17

---
+++ program-to-convert-celcius-in-farenheit-in.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int celcius;
+    cout<<"enter temperature in celcius = "<<endl;
+    cin>>celcius;
+    cout<<(1.8*celcius)+32<<"farenheit in "<<celcius<<"celcius";
+    return 0;
+    
+}
