360d7e7bba282f4d822d9fcaa720ea88eab5fef9
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:01:30 +0530

    10-april commit15

---
+++ program-to-perform-all-arithmetic-operations-in.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std ;
+int main()
+{
+    int a, b;
+    cout<<"Enter 1st number = "<<endl;
+    cin>>a;
+    cout<<"Enter 2nd number = "<<endl;
+    cin>>b;
+    cout<<"sum of both numbers = "<<a+b<<endl;
+    cout<<"Subtraction of both numbers = "<<a-b<<endl;
+    cout<<"multiplication of both numbers = "<<a*b<<endl;
+    cout<<"modulus of both numbers = "<<a%b<<endl;
+    cout<<"qoutient of both numbers = "<<a/b<<endl;
+    return 0;
+}
