2d7d894f423c9d796d60002068cc604a7f0bfcbf
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:07:28 +0530

    10-april commit21

---
+++ program-to-calculate-area-of-square-in.cpp
@@ -0,0 +1,11 @@
+//C++ Program to Calculate Area of Square.
+#include<iostream>
+using namespace std;
+int main()
+{
+    float side;
+    cout<<"Enter the side of the  Square = "<<endl;
+    cin>>side;
+    cout<<side*side<<" is the Area of the Square.";
+    return 0;
+}
