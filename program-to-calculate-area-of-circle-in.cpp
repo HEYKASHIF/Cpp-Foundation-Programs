77ef0d9f14a8ecbfade576a0d7fd52e8f8f7ac4b
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:06:38 +0530

    10-april commit20

---
+++ program-to-calculate-area-of-circle-in.cpp
@@ -0,0 +1,11 @@
+//C++ Program to Calculate Area of Circle.
+#include<iostream>
+using namespace std;
+int main()
+{
+    float rad;
+    cout<<"Enter the radius of the circle = "<<endl;
+    cin>>rad;
+    cout<<3.14*rad*rad<<" is the area of the Circle.";
+    return 0;
+}
