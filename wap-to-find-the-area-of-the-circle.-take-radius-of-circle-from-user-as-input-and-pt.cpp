1538b5668e2c50afbe231d64abca22cc24bfb27e
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:19:57 +0530

    10-april commit30

---
+++ wap-to-find-the-area-of-the-circle.-take-radius-of-circle-from-user-as-input-and-pt.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r;
+    printf("Enter the radius of the Circle = ");
+    scanf("%d",&r);
+    printf("%f",3.14*r*r);
+    return 0;
+}
