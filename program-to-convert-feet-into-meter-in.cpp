cfa97308beca6ef9b48200c21af7caff419df4a0
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:03:02 +0530

    10-april commit16

---
+++ program-to-convert-feet-into-meter-in.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int feet;
+    cout<<"enter the feet to convert into meter = "<<endl;
+    cin>>feet;
+    cout<<feet*0.3048<<"meter in "<<feet<<" feet";
+    return 0;
+}
