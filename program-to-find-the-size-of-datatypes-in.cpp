9a56c87d8f560f68c25a9f79e060e5f92df8a891
HEYKASHIF <kashifmohd269@gmail.com>
2023-04-10 01:05:32 +0530

    10-april commit19

---
+++ program-to-find-the-size-of-datatypes-in.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"size of int = "<<sizeof(int)<<endl;
+    cout<<"size of char = "<<sizeof(char)<<endl;
+    cout<<"size of float = "<<sizeof(float)<<endl;
+    cout<<"size of double = "<<sizeof(double)<<endl;
+    cout<<"size of bool = "<<sizeof(bool)<<endl;
+    cout<<"size of short = "<<sizeof(short)<<endl;
+    cout<<"size of long = "<<sizeof(long)<<endl;
+    cout<<"size of string = "<<sizeof(string)<<endl;
+    return 0;
+}
